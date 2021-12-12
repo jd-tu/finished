732136acb23651044c891193b35631231fc4654f
jd-tu <b00138460@mytudublin.ie>
2021-12-12 23:04:02 +0000

    gfvdre

---
+++ Assets/Link.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour 
+{
+
+	public void OpenGithubPage()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://github.com/jd-tu/game");
+		#endif
+	}
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
